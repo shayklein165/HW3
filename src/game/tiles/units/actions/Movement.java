e7e8aa2344954b7276177da7ed6d4f1ea0760966
shayklein165 <shaylior@post.bgu.ac.il>
2025-06-03 13:13:08 +0300

    a

--- src/game/tiles/units/actions/Movement.java
+++ src/game/tiles/units/actions/Movement.java
@@ -1,8 +1,80 @@
 package game.tiles.units.actions;
 
-public class Movement {
-    int enemyP
-    public Movement() {
+import game.utils.Position;
 
+import java.util.Random;
+
+public class Movement {
+    private Position enemyPostition;
+    private Position playerPostition;
+    private double distance;
+    private int enemyRange;
+
+    public Movement(Position enemyPosition, Position playerPosition, int enemyRange) {
+        this.enemyPostition = enemyPosition;
+        this.playerPostition = playerPosition;
+        this.distance = this.enemyPostition.Range(this.playerPostition);
+        this.enemyRange = enemyRange;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public Position getPlayerPostition(){
+        return this.playerPostition;
+    }
+
+    public Position getEnemyPostition(){
+        return this.enemyPostition;
+    }
+
+    public void setPlayerPostition(Position playerPosition){
+        this.playerPostition = playerPosition;
+    }
+
+    public void setEnemyPostition(Position enemyPostition){
+        this.enemyPostition = enemyPostition;
+    }
+
+    public void setDistance(double distance){
+        this.distance = distance;
+    }
+
+
+    public int Move(){
+        // need to check if there is a wall. this method only choose a new movement.
+        this.distance = this.enemyPostition.Range(this.playerPostition);
+        int move;
+        if(distance<enemyRange){
+            move = this.followPlayer();
+        }
+        else{
+            move = (int) (Math.random()*4);
+        }
+        return move;
+    }
+
+    // 0 = left, 1 = right, 2 = up, 3 = down
+    public int followPlayer(){
+        int distX = this.enemyPostition.getX()-this.playerPostition.getX();
+        int distY = this.enemyPostition.getY()-this.playerPostition.getY();
+
+        if(Math.abs(distX) > Math.abs(distY)){
+            if(distX > 0){
+                return 0;
+            }
+            else{
+                return 1;
+            }
+        }
+        else{
+            if(distY > 0){
+                return 2;
+            }
+            else{
+                return 3;
+            }
+        }
     }
 }
--- src/game/tiles/units/enemies/Monster.java
+++ src/game/tiles/units/enemies/Monster.java
@@ -1,4 +1,59 @@
 package game.tiles.units.enemies;
 
-public class Monster {
+import game.tiles.units.Energy;
+import game.tiles.units.actions.Movement;
+import game.utils.Position;
+
+public class Monster extends Energy {
+    private Movement movement;
+    private int range;
+    private Position position;
+
+    public Monster(int cost, int specialAbilityCost, int currentEnergy,int range, Position playerPosition, Position position) {
+        super(cost, specialAbilityCost, currentEnergy);
+        this.range = range;
+        this.position = position;
+        movement = new Movement(position, playerPosition, range);
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Movement getMovement() {
+        return movement;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public void setMovement(Movement movement) {
+        this.movement = movement;
+    }
+
+    public void OnEnemyTurn(){
+        int move = movement.Move();
+
+        if(move == 0){
+            this.position.setX(this.position.getX() + 1);
+        }
+        else if(move == 1){
+            this.position.setX(this.position.getX() - 1);
+        }
+        else if(move == 2){
+            this.position.setY(this.position.getY() + 1);
+        }
+        else if(move == 3){
+            this.position.setY(this.position.getY() - 1);
+        }
+    }
 }
