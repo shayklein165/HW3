25e0d8daed90cfa97648e37a743888de4c508156
shayklein165 <shaylior@post.bgu.ac.il>
2025-06-10 19:45:43 +0300

    fg

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String s = "#################################################";
-        System.out.println(s.length());
-    }
-}
--- src/game/tiles/units/actions/CastAbility.java
+++ src/game/tiles/units/actions/CastAbility.java
@@ -10,25 +10,6 @@ import java.util.List;
 
 public class CastAbility {
 
-    public CastAbility(ArrayGameBoard arrayGameBoard) {
+    public CastAbility() {
     }
-
-    public void executeAbility(Player player) {
-        List<Enemy> enemies = Level.;
-        if(enemies.isEmpty()){
-            return;
-        }
-
-        for(Enemy e: enemies){
-            int damage = 10 + player.getLevel(); // Example of scaling. need to be changed.
-            e.reciveDamage(damage);
-        }
-
-        player.setMana(player.getMana() - 10); // Example of cost. need to be changed.
-    }
-
-    public boolean isAbleToCast(Player player){
-        return player.getMana() >= 10 && !player.SelectEnemyInRange().isEmpty();
-    }
-
 }
