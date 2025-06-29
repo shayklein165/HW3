5e413dfb00ae9a27ea485eabb5b82409f51df3dc
shayklein165 <shaylior@post.bgu.ac.il>
2025-06-29 16:41:46 +0300

    Dfgdf

--- src/BoardTests/GeneralTests.java
+++ src/BoardTests/GeneralTests.java
@@ -33,38 +33,60 @@ public class GeneralTests {
         parser.parseLevel(files[0]);
         level = parser.interpret();
         board = level.getArrayGameBoard();
-
-        for(Enemy e: board.getEnemies()){
-            e.setDefense(0);
-        }
     }
 
+
     @Test
-    public void testWarriorLevelUp(){
+    public void testPlayerLevelUp(){
         this.player = new Warrior("Test player", new Position(2,2), 300, 30, 4, 3);
         this.player.gainExperience(50);
-        //asserts
+        assertEquals(0, this.player.getExperience());
+        assertEquals(2, this.player.getLevel());
+    }
+    @Test
+    public void testWarriorLevelUp(){
+        Warrior warrior = new Warrior("Test player", new Position(2,2), 300, 30, 4, 3);
+        this.player = warrior;
+        this.player.gainExperience(50);
+        assertEquals(330, warrior.getMaxHp());
+        assertEquals(0, warrior.getRemainingColldown());
+        assertEquals(42,warrior.getAttack());
+        assertEquals(8, warrior.getDefense());
     }
 
     @Test
     public void testMageLevelUp(){
-        this.player = new Mage("Test player", new Position(2,2), 100, 5, 1, 300, 30, 15, 5, 6);
+        Mage mage = new Mage("Test player", new Position(2,2), 100, 5, 1, 300, 30, 15, 5, 6);
+        this.player = mage;
         this.player.gainExperience(50);
-        //asserts
+        assertEquals(120, mage.getMaxHp());
+        assertEquals(13, mage.getAttack());
+        assertEquals(3, mage.getDefense());
+        assertEquals(350, mage.getManaPool());
+        assertEquals(162, mage.getMana());
+        assertEquals(35, mage.getSpellpower());
     }
 
     @Test
     public void testRogueLevelUp(){
-        this.player = new Rogue("Test player", new Position(2,2), 150, 40, 2, 20);
+        Rogue rogue = new Rogue("Test player", new Position(2,2), 150, 40, 2, 20);
+        this.player = rogue;
         this.player.gainExperience(50);
-        //asserts
+        assertEquals(170, rogue.getMaxHp());
+        assertEquals(54, rogue.getAttack());
+        assertEquals(4, rogue.getDefense());
+        assertEquals(100, rogue.getCurrentEnergy());
     }
 
     @Test
     public void testHunterLevelUp(){
-        this.player = new Hunter("Test player", new Position(2,2), 220, 30, 2, 6);
+        Hunter hunter = new Hunter("Test player", new Position(2,2), 220, 30, 2, 6);
+        this.player = hunter;
         this.player.gainExperience(50);
-        //asserts
+        assertEquals(42, hunter.getAttack());
+        assertEquals(6, hunter.getDefense());
+        assertEquals(240, hunter.getMaxHp());
+        assertEquals(30, hunter.getArrowscnt());
     }
 
 
